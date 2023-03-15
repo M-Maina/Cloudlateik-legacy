ecc498db251b53d35e93bb9e5c9df51bdefd6521
wamah <pythondeveloper21@gmail.com>
2023-03-15 11:41:12 +0000

    Added mysql variables in settings.py

--- storefront/settings.py
+++ storefront/settings.py
@@ -87,8 +87,11 @@ INTERNAL_IPS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Tronic@123'
     }
 }
 
