06e4769f278b328a5f12e6d3c40d1a4b96dd66e3
wamah <pythondeveloper21@gmail.com>
2023-03-15 22:27:59 +0000

    Using selected_related('').all()

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Customer
-from django.db.models import Q
+from django.db.models import Q, F
 
 # Create your views here.
 
 
 def index(request):
-    customer = Customer.objects.filter(Customer.email)
-    print(customer)
+    #customer = Customer.objects.filter(Customer.email)
     
     #To obtain greater than in queries eg price__gt=20, price__lt=20, price__eq=20
     
@@ -21,10 +20,24 @@ def index(request):
     #     pass
     
     #product = Customer.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
+    
+    #product = Customer.objects.filter(first_name=F('email'))
+    
     #product = Customer.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
     
     
     
+    #customer = Customer.objects.order_by('email')
+    #customer = Customer.objects.earliest('phone')
+    
+    #queryset = Customer.objects.all()[:25]
+    #queryset = Customer.objects.values('id', 'first_name')[:25]
+    
+    queryset = Customer.objects.only('id', 'last_name')[:25]
+    
+    query
+    
+    
     # for product in query_set:
     #     print(product)
-    return render(request, 'playground/hello.htm', {'customers':list(customer)})
+    return render(request, 'playground/hello.htm', {'customers': queryset })
